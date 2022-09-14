2e4e51f53731e454e67e274fc06fc56474d8f06f
methode <tmmethode1@gmail.com>
2022-09-14 06:49:38 +0200

    updates

--- banking_system/settings.py
+++ banking_system/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
@@ -141,4 +140,3 @@ CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
 CELERY_TIMEZONE = TIME_ZONE
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-django_heroku_settings(locals())
